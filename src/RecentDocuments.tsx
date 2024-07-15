2ff1462b6d47cb62457c1344a0978581062073fc
Kasper Høg <mailkasperhog@gmail.com>
2024-07-15 23:58:16 +0200

    updated Recent documents

--- src/RecentDocuments.tsx
+++ src/RecentDocuments.tsx
@@ -1,111 +1,70 @@
-import { exec } from "child_process";
-import { showHUD, ActionPanel, List, Icon, Action, closeMainWindow } from "@raycast/api";
+import { List } from "@raycast/api";
 import { useState, useEffect } from "react";
-import { promises as fsPromises } from "fs";
-import path from "path";
+import { fetchAllDocuments } from "./utils/document";
+import { addTagToDocument, removeTagFromDocument, getAllTags } from "./utils/tags";
+import DocumentItem from "./components/DocumentItem";
 
-const documentDirectories = [
-  "/Users/kasperhog/Downloads",
-  "/Users/kasperhog/Documents",
-  "/Users/kasperhog/Library/Mobile Documents/com~apple~CloudDocs",
-];
-
-type DocumentInfo = {
-  timestamp: number;
-  path: string;
-};
-
-const fetchDocumentsInFolder = async (folderPath: string): Promise<DocumentInfo[]> => {
-  try {
-    const documentPaths = await fsPromises.readdir(folderPath, { withFileTypes: true });
-    const docxFiles = documentPaths
-      .filter((dirent) => dirent.isFile() && dirent.name.endsWith(".docx") && !dirent.name.startsWith("~$"))
-      .map((dirent) => dirent.name);
-
-    const documents = await Promise.all(
-      docxFiles.map(async (filename) => {
-        const filePath = path.join(folderPath, filename);
-        const stats = await fsPromises.stat(filePath);
-        return { timestamp: stats.mtimeMs, path: filePath };
-      }),
-    );
-
-    const subfolderDocuments = (
-      await Promise.all(
-        documentPaths
-          .filter((dirent) => dirent.isDirectory())
-          .map((dirent) => fetchDocumentsInFolder(path.join(folderPath, dirent.name))),
-      )
-    ).flat();
-
-    return [...documents, ...subfolderDocuments];
-  } catch (error) {
-    console.error(`Error fetching documents in ${folderPath}: ${error}`);
-    return [];
-  }
-};
-
-const fetchAllDocuments = async (): Promise<DocumentInfo[]> => {
-  const documents = (await Promise.all(documentDirectories.map(fetchDocumentsInFolder))).flat();
-  return documents.sort((a, b) => b.timestamp - a.timestamp);
-};
-
-export default function Main() {
-  const [documentPaths, setDocumentPaths] = useState<DocumentInfo[]>([]);
+export default function RecentDocuments() {
+  const [documents, setDocuments] = useState<Document[]>([]);
   const [searchQuery, setSearchQuery] = useState("");
-  const limit = 16;
+  const [tagsFilter, setTagsFilter] = useState<string[]>([]);
+  const [allTags, setAllTags] = useState<string[]>([]);
 
   useEffect(() => {
-    fetchAllDocuments().then((allDocuments) => setDocumentPaths(allDocuments.slice(0, limit)));
+    const fetch = async () => {
+      const allDocuments = await fetchAllDocuments();
+      setDocuments(allDocuments);
+      const tags = await getAllTags();
+      setAllTags(tags);
+    };
+    fetch();
   }, []);
 
-  const loadMoreDocuments = async () => {
-    try {
-      const additionalDocuments = await fetchAllDocuments();
-      setDocumentPaths((prevDocuments) => [
-        ...prevDocuments,
-        ...additionalDocuments.slice(prevDocuments.length, prevDocuments.length + limit),
-      ]);
-    } catch (error) {
-      showHUD("Error loading more documents");
-    }
+  const handleTagAdded = (documentPath: string, tag: string) => {
+    setDocuments((prevDocs) =>
+      prevDocs.map((doc) => (doc.path === documentPath ? { ...doc, tags: [...doc.tags, tag] } : doc)),
+    );
   };
 
-  const filteredDocuments = documentPaths.filter((document) =>
-    document.path.toLowerCase().includes(searchQuery.toLowerCase()),
-  );
+  const handleTagRemoved = (documentPath: string, tag: string) => {
+    setDocuments((prevDocs) =>
+      prevDocs.map((doc) => (doc.path === documentPath ? { ...doc, tags: doc.tags.filter((t) => t !== tag) } : doc)),
+    );
+  };
+
+  const filteredDocuments = documents
+    .filter(
+      (document) =>
+        document.path.toLowerCase().includes(searchQuery.toLowerCase()) &&
+        (tagsFilter.length === 0 || tagsFilter.every((tag) => document.tags.includes(tag))),
+    )
+    .sort((a, b) => b.timestamp - a.timestamp);
 
   return (
-    <List searchBarPlaceholder="Search documents" onSearchTextChange={(text) => setSearchQuery(text)}>
+    <List
+      searchBarPlaceholder="Search documents"
+      onSearchTextChange={setSearchQuery}
+      searchBarAccessory={
+        <List.Dropdown
+          tooltip="Filter by Tag"
+          storeValue
+          onChange={(newValue) => setTagsFilter(newValue ? [newValue] : [])}
+        >
+          <List.Dropdown.Item title="All" value="" />
+          {allTags.map((tag) => (
+            <List.Dropdown.Item key={tag} title={tag} value={tag} />
+          ))}
+        </List.Dropdown>
+      }
+    >
       {filteredDocuments.map((document) => (
-        <List.Item
+        <DocumentItem
           key={document.path}
-          title={path.basename(document.path)}
-          subtitle={`Modified: ${new Date(document.timestamp).toLocaleString()}`}
-          icon="../assets/word.png"
-          actions={
-            <ActionPanel>
-              <Action
-                title="Open in Finder"
-                icon={Icon.Finder}
-                onAction={() => {
-                  exec(`open "${document.path}"`);
-                  closeMainWindow();
-                }}
-              />
-            </ActionPanel>
-          }
+          document={document}
+          onTagAdded={handleTagAdded}
+          onTagRemoved={handleTagRemoved}
         />
       ))}
-      <List.Item
-        title="Load More"
-        icon={Icon.Repeat}
-        actions={
-          <ActionPanel>
-            <Action title="Load More" icon={Icon.Repeat} onAction={loadMoreDocuments} />
-          </ActionPanel>
-        }
-      />
     </List>
   );
 }
